58f7170ca584d1d86429700fb807dd141886e21d
artem <artemmikhalitsin@gmail.com>
2017-08-09 09:10:21 -0400

    First Commit

---
+++ includes/connection.php
@@ -0,0 +1,13 @@
+<?php
+include('env.php');
+
+//create a connection
+$conn = mysqli_connect($server, $username, $password, $db);
+
+//check connection
+if( !$conn ){
+    die( "Connection failed: " . mysqli_connect_error() );
+}
+
+
+?>
